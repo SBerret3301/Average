1ed0fead255f85d02e5397c0790d7b35795235dc
salah_eddine_berret <salaheddineberret@gmail.com>
2023-10-05 19:57:11 +0100

    adding the algorithme to e15.py

--- e15.py
+++ e15.py
@@ -1,3 +1,55 @@
+#algorithme note
+#variable 
+#       n1 , n2 , n3 , m : reel
+#                 f : chaine de caracter
+#Debut
+#     Ecrire("tapez la 1 er note : ")
+#     Lire(n1)
+#     Ecrire("tapez la 2 eme note : ")
+#     Lire(n2)
+#     Ecrire("tapez la 3 eme note : ")
+#     Lire(n3)
+#     m= (n1 + n2 + n3)/3
+#     tant que n1 > 20 ou n1 < 0 faire
+#         Ecrire("tapez correctement la 1 er note : ")
+#         Lire(n1)
+#     fin tant que
+#     tant que n2 > 20 ou n2 < 0 faire
+#         Ecrire("tapez correctement la 2 eme note : ")
+#         Lire(n2)
+#     fin tant que
+#     tant que n3 > 20 ou n3 < 0 faire
+#         Ecrire("tapez correctement la 3 eme note : ")
+#         Lire(n3)
+#     fin tant que
+#     si m >= 16 alors
+#        f <-- "tres bien"
+#     sinon
+#         si m < 16 et m >= 14 alors
+#            f <-- "bien"
+#         sinon
+#             si m < 14 et m >= 12 alors
+#                f <-- "assez bien"
+#             sinon
+#                 si m < 12 et m >= 10 alore
+#                    f <-- "passable"
+#                 sinon
+#                     si m < 10 alors
+#                         f <-- "insuffisant"
+#                     fin_si
+#                 fin_si
+#             fin_si
+#         fin_si
+#     fin_si
+#     Ecrire("la moyenne est : ",m,"il est : ",f)
+
+
+
+
+
+
+
+
 n1 = float(input("enter the first mark : "))
 n2 = float(input("enter the second mark : "))
 n3 = float(input("enter the third mark : "))
